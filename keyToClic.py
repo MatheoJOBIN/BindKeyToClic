b9626574a33711a0bf7aaff8778a8af1ecb2b810
MatheoJOBIN <80678160+MatheoJOBIN@users.noreply.github.com>
2023-05-15 01:13:19 +0200

    Working, but latency

--- keyToClic.py
+++ keyToClic.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 import win32gui
 import win32api
 import win32con
 from pynput import keyboard
 from pywinauto import Application
+import tkinter as tk
 
 def get_window_client_rect(window_title):
     hwnd = win32gui.FindWindow(None, window_title)
@@ -35,17 +37,28 @@ def click_on_border(window_title, border):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
-def on_press(key):
+def on_press_wrapper(window_titles):
+    def on_press(key):
+        on_press_real(key, window_titles)
+    return on_press
+
+def on_press_real(key, window_titles):
+    app = Application(backend="uia")
     if key == keyboard.KeyCode(char='²'):
         # Stop listener when 'Esc' key is pressed
         print('Exiting...')
         return False
     elif key == keyboard.Key.left:
+        print('Left key pressed')
         for window_title in window_titles:
-            app = Application(backend="uia").connect(title_re=window_title)
+            app.connect(title_re=window_title)
+            print('1')
             window = app.window(title_re=window_title)
+            print('2')
             window.set_focus()
+            print('3')
             click_on_border(window_title, 'left')
+            print('Left click performed')
     elif key == keyboard.Key.right:
         for window_title in window_titles:
             app = Application(backend="uia").connect(title_re=window_title)
@@ -64,14 +77,45 @@ def on_press(key):
             window = app.window(title_re=window_title)
             window.set_focus()
             click_on_border(window_title, 'bottom')
+    else:
+        print('Key pressed: ' + str(key))
 
-# Specify the titles of the application windows you want to target
-window_titles = ['Scoupsoin - Dofus 2.67.10.13', 'Scoupp - Dofus 2.67.10.13']
+def execute_script(window_titles):
+    # Create a listener for keyboard inputs
+    listener = keyboard.Listener(on_press=on_press_wrapper(window_titles))
+    # Start the listener in a separate thread
+    listener.start()
 
-# Create a listener for keyboard inputs
-listener = keyboard.Listener(on_press=on_press)
-# Start the listener in a separate thread
-listener.start()
+    # Keep the main thread running
+    listener.join()
 
-# Keep the main thread running
-listener.join()
+def submit_parameters():
+    titles = entry_window_titles.get().split(',')
+    window_titles = [title.strip() for title in titles]
+    print("Window Titles: " + str(window_titles))
+    execute_script(window_titles)
+
+# Initialize the variables
+window_titles = []
+
+# Create the GUI window
+window = tk.Tk()
+window.title("Parameter Input")
+window.geometry("300x150")
+
+# Window Titles Label and Entry
+label_window_titles = tk.Label(window, text="Window Titles (comma-separated):")
+label_window_titles.pack()
+entry_window_titles = tk.Entry(window)
+entry_window_titles.pack()
+
+# Submit Button
+submit_button = tk.Button(window, text="Submit", command=submit_parameters)
+submit_button.pack()
+
+# Quit Button
+quit_button = tk.Button(window, text="Quit", command=window.quit)
+quit_button.pack()
+
+# Start the GUI event loop
+window.mainloop()
